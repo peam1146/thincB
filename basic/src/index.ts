eb85b27d280cb99cb710a0956688447f33e4b12f
Peam <peammy2010@hotmail.com>
2022-03-11 15:46:37 +0700

    basic syntax

---
+++ basic/src/index.ts
@@ -0,0 +1,24 @@
+// if else
+var i = 0;
+if (true) {
+  i = 1;
+} else {
+  i = 2;
+}
+
+var j = i == 2 ? 1 : 2;
+
+console.log(i, j);
+
+// while loop
+var i = 1;
+while (i < 10) {
+  console.log(i);
+  i++; // i = i + 1
+}
+
+//for loop
+for (let i = 0; i <= 10; i++) {
+  // for i in range(10)
+  console.log(i);
+}
